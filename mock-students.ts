a852e098772320993e473b44be71101b5d62e653
Dominik Črnjak <101872948+DominikCrnjak@users.noreply.github.com>
2023-09-08 16:59:44 +0200

    Delete mock-students.ts

--- mock-students.ts
+++
@@ -1,6 +0,0 @@
-import { Student } from './student';
-export const STUDENTS: Student[] = [
-{ jmbag: "0243235445", numberOfECTS: 120, tuition: true },
-{ jmbag: "0243235337", numberOfECTS: 180, tuition: false },
-{ jmbag: "0243234432", numberOfECTS: 300, tuition: false }
-];
